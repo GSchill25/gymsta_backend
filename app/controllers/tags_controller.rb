cb8b548e3e49d19fa9aae2ea1add4d739b1a9980
GSchill25 <schill1025@gmail.com>
2016-12-17 15:18:26 -0500

    update tags on Workout

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
   def index
-    render json: { tags: Article.tag_counts.most_used.map(&:name) }
+    render json: { tags: Workout.tag_counts.most_used.map(&:name) }
   end
 end
